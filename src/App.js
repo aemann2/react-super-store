f7fc8635db0ac1e2549f854551fb1edd548a3790
Adam Mann <aemann2@gmail.com>
2021-04-05 18:26:23 -0700

    added a search bar component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './css/style.css';
 import axios from 'axios';
 import Navbar from './components/layout/Navbar';
+import Search from './components/search/Search';
 import Home from './components/pages/Home';
 import Deals from './components/pages/Deals';
 import Cart from './components/pages/Cart';
@@ -30,6 +31,7 @@ function App() {
     <Router>
       <div className='App'>
         <Navbar />
+        <Search />
         {/* this is the current recommended pattern for creating routes in React */}
         <Switch>
           <Route exact path='/'>
